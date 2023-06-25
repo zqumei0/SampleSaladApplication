04ec50911e28e49feb73adc5c92ba2f820ea548b
zqumei <zaid.qumei+os@gmail.com>
2023-06-24 23:32:51 -0400

    Initial commit

---
+++ test/example_salad_app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExampleSaladApp from '../lib/example_salad_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example_salad_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExampleSaladApp.ExampleSaladAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
